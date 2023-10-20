0683b79b41436344d00a35582a93b538af937468
dmz4484 <daniazein20@gmail.com>
2023-10-20 16:19:56 -0500

    new update

--- Hello.py
+++ Hello.py
@@ -1,5 +1,5 @@
 def hello_world():
-    print("Hello, World!")
+    print("My name is Dania")
 
 def print_something():
     print("Something.")
@@ -7,6 +7,7 @@ def print_something():
 def main():
     hello_world()
     print_something()
+    print(hello_world())
 
 if __name__ == "__main__":
     main()
