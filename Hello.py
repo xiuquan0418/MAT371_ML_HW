362c7a02adce8c3e92b3146d9c0353a24c37d6bd
xiuquan0418 <xiuquan0418@gmail.com>
2023-10-20 16:22:58 -0500

    updated

--- Hello.py
+++ Hello.py
@@ -2,11 +2,12 @@ def hello_world():
     print("Hello, World!")
 
 def print_something():
-    print("Something.")
+    print("This is Dania")
 
 def main():
     hello_world()
     print_something()
+    print(print_something())
 
 if __name__ == "__main__":
     main()
