eb3e07ab5abfa8ded8be945f063841c2bbaab9d1
xiuquan0418 <xiuquan0418@gmail.com>
2023-10-20 16:09:57 -0500

    add Hello.py

---
+++ Hello.py
@@ -0,0 +1,13 @@
+def hello_world():
+    print("Hello, World!")
+
+def print_something():
+    print("Something.")
+
+def main():
+    hello_world()
+    print_something()
+
+if __name__ == "__main__":
+    main()
+
